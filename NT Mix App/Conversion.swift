69ee75f204013856e83d47a57c2a0d4f9eb8ce95
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2015-11-12 09:21:33 -0700

    Original Creation of App

---
+++ NT Mix App/Conversion.swift
@@ -0,0 +1,13 @@
+//
+//  Conversion.swift
+//  NT Mix App
+//
+//  Created by Jeremy Skrdlant on 11/12/15.
+//  Copyright © 2015 Jeremy Skrdlant. All rights reserved.
+//
+
+import UIKit
+
+class Conversion: NSObject {
+
+}
