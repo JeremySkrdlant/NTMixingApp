5e40484676bb9152df1e34fcff2bfd50ea56c25d
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2015-11-12 14:13:33 -0700

    Added base Storyboard to project

---
+++ NT Mix App/CalculationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalculationViewController.swift
+//  NT Mix App
+//
+//  Created by Jeremy Skrdlant on 11/12/15.
+//  Copyright © 2015 Jeremy Skrdlant. All rights reserved.
+//
+
+import UIKit
+
+class CalculationViewController: UIViewController {
+    @IBOutlet weak var tankVolumeInput: UITextField!
+    @IBOutlet weak var acresInput: UITextField!
+    @IBOutlet weak var applicationRateInput: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+
+    @IBAction func hideKeyboards() {
+        tankVolumeInput.resignFirstResponder()
+        acresInput.resignFirstResponder()
+        applicationRateInput.resignFirstResponder()
+    }
+ 
+
+
+}
+
