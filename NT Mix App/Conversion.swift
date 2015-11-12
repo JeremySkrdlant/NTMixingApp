f7aa8a994f0d9967d6fb22a394ac6e7a31223a9e
Xavier Colby <xaviercolby@xaviers-macbook-pro.local>
2015-11-12 10:57:36 -0700

    stuff

--- NT Mix App/Conversion.swift
+++ NT Mix App/Conversion.swift
@@ -31,14 +31,13 @@ extension Double{
     
     func cupsToGallons(roundAmount:Double)->Double{
         //raymond
-        return 0.0
+
         let result = self / 16.0
         return round(result * roundAmount) / roundAmount
     }
     
     func gallonsToCups(roundAmount:Double)->Double{
         //raymond
-        return 0.0
         let result = self * 16.0
         return round(result * roundAmount) / roundAmount
     }
