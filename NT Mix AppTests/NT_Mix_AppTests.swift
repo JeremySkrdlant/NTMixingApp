404d538bb92d409ef14ec03318dacc7440b8b548
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2015-11-12 10:49:30 -0700

    Added a few changes to the tests

--- NT Mix AppTests/NT_Mix_AppTests.swift
+++ NT Mix AppTests/NT_Mix_AppTests.swift
@@ -13,11 +13,9 @@ class NT_Mix_AppTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-
     }
     
     override func tearDown() {
-
         super.tearDown()
     }
     
@@ -25,12 +23,19 @@ class NT_Mix_AppTests: XCTestCase {
         let startAmount = 43.0;
         let result = startAmount.fluidOuncesToGallons(100)
         XCTAssert(result == 0.34, "It is converted correctly according to Siri")
+        let startTwo = 12.0;
+        let result2 = startTwo.fluidOuncesToGallons(1000)
+        XCTAssert(result2 == 0.094, "12 fl oz to gallons")
     }
     
     func testOuncesToPounds(){
         let startAmount = 15.0
         let result = startAmount.ouncesToPounds(100)
         XCTAssert(result == 0.94, "It is converted correctly according to Siri")
+        let start2 = 42.0
+        let result2 = start2.ouncesToPounds(10)
+        XCTAssert(result2 == 2.6, "42 ounces to pounds")
+        
     }
     func testCupsToGallons(){
         let startAmount = 45.0
