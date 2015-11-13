188ea72f2d58992fbc6b0ec966f7308e830a61cb
Raymond A. Eli <reli42@maed-4.local>
2015-11-13 09:13:02 -0700

    done the basic stuff

--- NT Mix App/CalculationViewController.swift
+++ NT Mix App/CalculationViewController.swift
@@ -12,6 +12,7 @@ class CalculationViewController: UIViewController {
     @IBOutlet weak var tankVolumeInput: UITextField!
     @IBOutlet weak var acresInput: UITextField!
     @IBOutlet weak var applicationRateInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
      
@@ -23,7 +24,32 @@ class CalculationViewController: UIViewController {
         applicationRateInput.resignFirstResponder()
     }
  
+    @IBAction func tankVolume() {
+        let tank = (tankVolumeInput.text! as NSString).doubleValue
+        let acress = (acresInput.text! as NSString).doubleValue
+        let appRate = (applicationRateInput.text! as NSString).doubleValue
+        if (tank == 0){
+            tankVolumeInput.text = "\(acress * appRate)"
+        }
+        
+    }
+    @IBAction func applicationRate() {
+        let tank = (tankVolumeInput.text! as NSString).doubleValue
+        let acress = (acresInput.text! as NSString).doubleValue
+        let appRate = (applicationRateInput.text! as NSString).doubleValue
+        if (appRate==0){
+            applicationRateInput.text = "\(tank/acress)"
+        }
+    }
 
-
+    @IBAction func acres() {
+        let tank = (tankVolumeInput.text! as NSString).doubleValue
+        let appRate = (applicationRateInput.text! as NSString).doubleValue
+        let acress = (acresInput.text! as NSString).doubleValue
+        if (acress == 0){
+            acresInput.text = "\(tank/appRate)"
+        }
+    }
 }
+
 
